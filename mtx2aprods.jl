05258d85d89a0cd91151577f8c5d6cb3ec99d3eb
andrebuhler <andre_buhler@hotmail.com>
2017-06-26 11:20:35 -0300

    Add files via upload

--- mtx2aprods.jl
+++ mtx2aprods.jl
@@ -30,8 +30,8 @@ function mtx2aprods(mtx :: String; compact = true)
         s[i] = s[i] * "$sgn$aij*v[$j]"
         st[j] = st[j] * "$sgn$aij*v[$i]"
       else
-        s[i] = s[i] * "$sgn $aij*v[$j]"
-        st[j] = st[j] * "$sgn $aij*v[$i]"
+        s[i] = s[i] * " $sgn $aij*v[$j]"
+        st[j] = st[j] * " $sgn $aij*v[$i]"
       end
     end
   end
@@ -45,34 +45,30 @@ function mtx2aprods(mtx :: String; compact = true)
         @printf(file_Aprod, "s[%d]=%s;", i, s[i])
       end
       write(file_Aprod,"];return s; end\n")
+
+      write(file_Aprod,"function Aprod(v)")
+      write(file_Aprod," st=[")
+      for j = 1:n
+        @printf(file_Aprod, "st[%d] = %s;", j, st[j])
+      end
+      write(file_Aprod,"];return st; end")
+
     else
-      write(file_Aprod,"\ns = [\n")
+      write(file_Aprod,"\n    s = [\n")
       for i = 1:m
-        @printf(file_Aprod, "s[%d] = %s\n", i, s[i])
+        @printf(file_Aprod, "        s[%d] = %s\n", i, s[i])
       end
-      write(file_Aprod,"]\nreturn s\n end\n")
+      write(file_Aprod,"    ]\n    return s\nend\n")
+
+      write(file_Aprod,"\nfunction Aprod(v)")
+      write(file_Aprod,"\n    st = [\n")
+      for j = 1:n
+        @printf(file_Aprod, "        st[%d] = %s\n", j, st[j])
+      end
+      write(file_Aprod,"    ]\n    return st\nend")
+
     end
 
   end
-
-  open("Aprod.jl", "a") do file_Atprod
-
-    write(file_Atprod,"function Atprod(v)")
-    if compact
-      write(file_Atprod," st=[")
-      for j = 1:n
-        @printf(file_Atprod, "st[%d] = %s;", j, st[j])
-      end
-      write(file_Atprod,"];return st; end")
-    else
-      write(file_Atprod,"\nst = [\n")
-      for j = 1:n
-        @printf(file_Atprod, "st[%d] = %s\n", j, st[j])
-      end
-      write(file_Atprod,"]\nreturn st\n end")
-    end
-  end
-
-
   return
 end
