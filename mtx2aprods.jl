3a49483da66ca0dce9f34832e9dfb7421787a81c
andrebuhler <andre_buhler@hotmail.com>
2017-06-30 21:19:28 -0300

    Update mtx2aprods.jl

--- mtx2aprods.jl
+++ mtx2aprods.jl
@@ -116,19 +116,19 @@ function mtx2aprods(mtx :: String; compact = true)
 
     open("Aprod.jl", "w") do file_Aprod
 
-        @printf(file_Aprod,"function Aprod(v,s)%s%ss%s=%s[",cchar2,cchar3,cchar1,cchar1);
+        @printf(file_Aprod,"function Aprod(Av,v)%s%sAv%s=%s[",cchar2,cchar3,cchar1,cchar1);
         for i = 1:m
             if s[i] != ""
                 @printf(file_Aprod, "%s%s",  s[i],cchar2);
             end
         end
-        @printf(file_Aprod,"]%s%sreturn s%send\n\nfunction Atprod(v,st)%s%sst%s=%s[",cchar2,cchar3,cchar2,cchar2,cchar3,cchar1,cchar1)
+        @printf(file_Aprod,"]%s%sreturn Av%send\n\nfunction Atprod(Av,v)%s%sAv%s=%s[",cchar2,cchar3,cchar2,cchar2,cchar3,cchar1,cchar1)
         for j = 1:n
             if st[j] !=""
                 @printf(file_Aprod, "%s%s", st[j],cchar2)
             end
         end
-        @printf(file_Aprod,"]%s%sreturn st%send",cchar2,cchar3,cchar2)
+        @printf(file_Aprod,"]%s%sreturn Av%send",cchar2,cchar3,cchar2)
     end
     println("Success")
     return (m,n,nz)
